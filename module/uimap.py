275ac1dc172bdb71792e9d3d4d3af41f91c483b8
Abhishta Gatya <abhishtagatya@yahoo.com>
2018-07-19 00:29:43 +0700

    Changes to API Weather ID #1

--- module/uimap.py
+++ module/uimap.py
@@ -164,14 +164,16 @@ ui_map = {
     }
 }
 
+
+# This maps the code ranges
 weather_code_range = [
-            (range(200,233), 'thunderstorm'),
-            (range(300, 322), 'rain'),
-            (range(500, 532), 'rain'),
-            (range(600, 622), 'snow'),
-            (range(701,782), 'mist'),
-            ([800], 'clear sky'),
-            ([801], 'few clouds'),
-            ([802], 'scattered clouds'),
-            ([803,804], 'broken clouds')
-            ]
+    (range(200,233), 'thunderstorm'),
+    (range(300, 322), 'shower rain'),
+    (range(500, 532), 'rain'),
+    (range(600, 622), 'snow'),
+    (range(701,782), 'mist'),
+    ([800], 'clear sky'),
+    ([801], 'few clouds'),
+    ([802], 'scattered clouds'),
+    ([803,804], 'broken clouds')
+]
